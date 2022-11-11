d4f24f98a7515d600e910f7a61956b203331f054
Muhammed Abdullahi <MABDULLAHI@unionbankng.com>
2022-11-11 08:37:38 +0100

    created the skeleton for sidebar and chat components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react';
+
 import './App.css';
+import {Chat} from './components';
+import {Sidebar} from './components';
+
 
 function App() {
   return (
     <div className="app">
-
+      <div className="app__body">
+        <Sidebar />
+        <Chat />
+      </div>
     </div>
   );
 }

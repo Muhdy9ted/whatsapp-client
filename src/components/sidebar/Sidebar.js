8f53a2cf8b494fba9c392446de015d30a98a049f
Muhammed Abdullahi <MABDULLAHI@unionbankng.com>
2022-11-11 09:20:04 +0100

    finished up sidebar header ui-build

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,8 @@
 import React from 'react'
+
+import {DonutLarge, MoreVert, Chat} from '@mui/icons-material';
+import { IconButton, Avatar } from '@mui/material';
+
 import './Sidebar.css'
 
 function Sidebar() {
@@ -7,8 +11,12 @@ function Sidebar() {
 
         <div className='sidebar__header'>
 
-            <div className='sidebar__headerRight'>
+            <Avatar src='https://avatars.githubusercontent.com/u/27841940?v=4' />
 
+            <div className='sidebar__headerRight'>
+                <IconButton> <DonutLarge /> </IconButton>
+                <IconButton> <Chat /> </IconButton>
+                <IconButton> <MoreVert /> </IconButton>
             </div>
 
         </div>
